aa524735535f2bd3f39f7d630ea54b0f4c88d6bf
Linus Lee <linus@thelifelongtraveler.com>
2016-04-20 19:49:33 -0400

    Works with google's new hash-based URI scheme

--- injected.js
+++ injected.js
@@ -1,11 +1,11 @@
 // pulling in local settings
 var dv_e, sh_e, qs_e, sh_content;
 chrome.storage.sync.get({
-    dv:true,
-    sh:true,
-    qs:true,
+    dv:true, 
+    sh:true, 
+    qs:true, 
     shc: "stackx wiki edux"
-},function(record){
+}, function(record){
     dv_e = record.dv;
     sh_e = record.sh;
     qs_e = record.qs;
@@ -15,29 +15,30 @@ chrome.storage.sync.get({
 
 // main loop
 function ms_script(){
+    
     typeHash = {
-        "img": "&tbm=isch",
-        "image": "&tbm=isch",
-        "news": "&tbm=nws",
-        "vid": "&tbm=vid",
-        "book": "&tbm=bks",
-        "translate": "_tl",
-        "tl": "_tl",
-        "yt": "_yt",
-        "sch": "_sch",
-        "wiki": "_wk",
-        "amz": "_amz",
-        "map": "_maps",
-        "maps": "_maps",
-        "pdf": "_pdf",
-        "ppt": "_pdf",
-        "verbatim": "&tbs=li:1",
-        "v": "&tbs=li:1",
-        "flickr": "_flickr",
-        "wa": "_wa",
-        "quotes": "_bq",
-        "quote": "_bq",
-        "urban": "_ub",
+        "img": "&tbm=isch", 
+        "image": "&tbm=isch", 
+        "news": "&tbm=nws", 
+        "vid": "&tbm=vid", 
+        "book": "&tbm=bks", 
+        "translate": "_tl", 
+        "tl": "_tl", 
+        "yt": "_yt", 
+        "sch": "_sch", 
+        "wiki": "_wk", 
+        "amz": "_amz", 
+        "map": "_maps", 
+        "maps": "_maps", 
+        "pdf": "_pdf", 
+        "ppt": "_pdf", 
+        "verbatim": "&tbs=li:1", 
+        "v": "&tbs=li:1", 
+        "flickr": "_flickr", 
+        "wa": "_wa", 
+        "quotes": "_bq", 
+        "quote": "_bq", 
+        "urban": "_ub", 
         "lucky": "_lucky"
     }
     
@@ -45,9 +46,9 @@ function ms_script(){
     function goToYT(query_string){window.location.href="https://www.youtube.com/results?search_query="+query_string;}
     function goToSch(query_string){window.location.href="https://scholar.google.com/scholar?q="+query_string;}
     function goToWiki(query_string){window.location.href="https://en.wikipedia.org/w/index.php?search="+query_string;}
-    function goToKey(query_string,key){window.location.href="https://www.google.com/search?q="+query_string+key;}
-    function goToAmz(query_string){query_string=query_string.replace(/\+/g,'%20');window.location.href="http://www.amazon.com/s/field-keywords="+query_string;}
-    function goToTl(query_string){query_string=query_string.replace(/\+/g,'%20');window.location.href="https://translate.google.com/#auto/en/"+query_string;}
+    function goToKey(query_string, key){window.location.href="https://www.google.com/search?q="+query_string+key;}
+    function goToAmz(query_string){query_string=query_string.replace(/\+/g, '%20');window.location.href="http://www.amazon.com/s/field-keywords="+query_string;}
+    function goToTl(query_string){query_string=query_string.replace(/\+/g, '%20');window.location.href="https://translate.google.com/#auto/en/"+query_string;}
     function goToMaps(query_string){window.location.href="https://www.google.com/maps/place/"+query_string;}
     function goToPpt(query_string){window.location.href="https://www.google.com/search?q="+query_string+"+filetype:ppt";}
     function goToPdf(query_string){window.location.href="https://www.google.com/search?q="+query_string+"+filetype:pdf";}
@@ -65,11 +66,12 @@ function ms_script(){
         
         inc = rv_url.indexOf("=q&");
         if(inc==-1){inc=rv_url.indexOf("=q?")}
+        if(inc==-1){inc=rv_url.indexOf("=q#")}
         if(inc==-1){return false}
         
         inc = ms_url.length - inc;
         
-        term = ms_url.substring(inc,ms_url.length);
+        term = ms_url.substring(inc, ms_url.length);
         term = term.split("&")[0];
         
         terml = term.split(":");
@@ -84,11 +86,11 @@ function ms_script(){
     }
     
     // go to target with query
-    function goToSearch(type,query){
+    function goToSearch(type, query){
         typeID = typeHash[type];
         if(typeID==undefined){return null}
         else if(typeID[0]=="&"){
-            goToKey(query,typeID);
+            goToKey(query, typeID);
         }else if(typeID[0]=="_"){
             if(typeID=="_yt"){
                 goToYT(query);
@@ -125,40 +127,41 @@ function ms_script(){
         search_data = queryPresent();
         if(typeof(search_data)=="object"){
             if(typeHash[search_data[0]]!=undefined){
-                document.write();goToSearch(search_data[0],search_data[1])
+                document.write();
+                goToSearch(search_data[0], search_data[1])
             }
         }
     }
     
     // when to check for query
-    if(qs_e==true){
+    if(qs_e){
         mssync();
-        window.addEventListener("hashchange",mssync);
     }
-    
+
     // when to enable dualview
     function dualview(){
+        console.log("processing dualview");
         if(document.getElementById("rhs_block")!=null && document.getElementById("cnt")!=null){
             if(document.getElementById("rhs_block").children[1]!=undefined){
                 if(document.getElementById("rhs_block").children[1].tagName.toLowerCase()=="script" || document.getElementById("rhs_block").children[1].tagName.toLowerCase()=="div"){
-                    document.getElementById("cnt").setAttribute("data-msdualviewenabled","true");}else{
-                        document.getElementById("cnt").setAttribute("data-msdualviewenabled","false");}
-            }else{document.getElementById("cnt").setAttribute("data-msdualviewenabled","true");}
-        }else{document.getElementById("cnt").setAttribute("data-msdualviewenabled","true");}
+                    document.getElementById("cnt").setAttribute("data-msdualviewenabled", "true");}else{
+                        document.getElementById("cnt").setAttribute("data-msdualviewenabled", "false");}
+            }else{document.getElementById("cnt").setAttribute("data-msdualviewenabled", "true");}
+        }else{document.getElementById("cnt").setAttribute("data-msdualviewenabled", "true");}
         document.getElementById('center_col').style.paddingTop=0;
     }
-    
+   
     // activate dualview
-    if(dv_e==true){
-        document.addEventListener("DOMContentLoaded",function(){
+    if(dv_e){
+        document.addEventListener("DOMContentLoaded", function(){
             dualview();
         });
-        document.addEventListener("keydown",function(){
+        document.addEventListener("keydown", function(){
             clearInterval(counterP);
-            counterP = setTimeout(dualview,900);
+            counterP = setTimeout(dualview, 700);
         })
     }
     
     // activate Search Highlight
-    if(sh_e==true){document.addEventListener("DOMContentLoaded",function(){document.body.setAttribute("data-mshighlight",sh_content);})}
+    if(sh_e){document.addEventListener("DOMContentLoaded", function(){document.body.setAttribute("data-mshighlight", sh_content);})}
 };
